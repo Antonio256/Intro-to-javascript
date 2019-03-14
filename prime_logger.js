75c5d155acfa1a9224d1b8bd02da617afd2c6cad
Anthony Atwine <anthonybakunda@gmail.com>
2019-03-14 08:47:10 +0300

    adding FizzBuzz file

---
+++ prime_logger.js
@@ -0,0 +1,12 @@
+//Enter a number
+//Entered number when devided by 2 leaves a remainder
+//Or when devided by 3 leaves a remainder
+//print the number
+
+var n;
+
+for (n = 30; n<= 30; n++){
+  if (n % 2 == 0) {
+    console.log(n);
+  }
+}
