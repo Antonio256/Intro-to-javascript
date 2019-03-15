9cf007929d1c00ca6e31656d72a678be37d23399
Anthony Atwine <anthonybakunda@gmail.com>
2019-03-15 14:07:46 +0300

    add log_evens

--- log_evens.js
+++ log_evens.js
@@ -1,7 +1,19 @@
-//Input the variable
-var a ;
+/*
 
-for (a = 1; a<=20; a++){
-if (a % 2 == 0)
-console.log(a);
+crate a function using a constant
+set the range of numbers as min and max
+input numbers within the range
+count numbers from min to max
+if number is divisible by 2, print the number 
+*/
+
+
+const even = (min, max) => {
+  for (a = min; a<=max; a++){
+  if (a % 2 === 0){
+    console.log(a);
+  }
+
+  }
 }
+console.log(even(1, 20));
