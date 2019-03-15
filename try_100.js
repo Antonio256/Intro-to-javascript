af87eed5e3c196eb364178f8607a3b3a0a64c77d
Anthony Atwine <anthonybakunda@gmail.com>
2019-03-15 14:18:51 +0300

    add try_100

--- try_100.js
+++ try_100.js
@@ -1,27 +1,17 @@
-//Enter any number greater than 100
-//when number is greater than 100
-//print ok
-//when number is less than 100
-//enter again
+/*Enter any number
+when number is less than 100, print try again
+when number is greater than 100, print OKAY
+print ok
+enter again
+*/
 
 
-//console.log("Please input a number greater than 100")
+const num = number => {
+  if (number<100){
+    console.log("TRY AGAIN");
+  }else {
+    console.log("OKAY!")
+}
+}
 
-/*var n = 567;
-
-if (n<100){
-  console.log("TRY AGAIN");
-}else {
-  console.log("OKAY!")
-}*/
-
-
-const num = (num) =>
-(num>100);
-"okay"
-
-
-
-//  console.log("OKAY!")
-
-console.log(num(90));
+console.log(num(200));
